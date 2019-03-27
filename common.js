885672c34049f8034fa0f993a54b92691c6b1b19
yumcc <yumcc@qq.com>
2019-03-27 21:18:57 +0800

    增加可控制的缩放图片功能

--- common.js
+++ common.js
@@ -1,15 +1,23 @@
 const request = require('request')
 const fs = require('fs')
 const mkdirp = require('mkdirp')
+const gm = require('gm').subClass({ imageMagick: true })
+const images = require("images")
 
 class Ut {
   /**
 	 * 下载网络图片
-	 * @param {object} opts 
+	 * @param {object} opts
+   * @param {string} path
+   * @param {string} dir_path
+   * @param {string} type_folder
+   * @param {object} size_data
+   * @param {boolean} is_resize
+   * @param {string} id
 	 */
-  static downImg(opts = {}, path = '', dirpath = '') {
+  static downImg(opts = {}, path = '', dir_path = '', type_folder, size_data, is_resize, id) {
     return new Promise((resolve, reject) => {
-      mkdirp(dirpath, function (err) {
+      mkdirp(dir_path, (err) => {
         if (err) {
           console.error(err)
           reject()
@@ -27,16 +35,53 @@ class Ut {
               resolve('')
             })
             .on('finish', () => {
-              console.log('finish')
+              if (is_resize) {
+                resizeCurrentImg(path, type_folder, size_data[type_folder]).then(() => {
+                  resolve('finish')
+                  console.log(`ID为${id}的${type_folder}图片下载并缩放完成`)
+                }).catch(err => {
+                  reject(err)
+                })
+              } else {
+                resolve('finish')
+                console.log(`ID为${id}的${type_folder}图片下载完成`)
+              }
             })
             .on('close', () => {
               console.log('close')
-              resolve('close')
             })
         }
       })
     })
   }
+}
+
+// 缩放图片(swipe图片为正方形同高同宽，content为同宽不同高需要同比例缩放计算)
+/**
+ * 下载网络图片
+ * @param {string} srcImg
+ * @param {string} type_folder
+ * @param {object} size_data
+ */
+let resizeCurrentImg = async (srcImg, type_folder, size_data) => {
+  return new Promise(async (resolve, reject) => {
+    let width = await images(srcImg).width()
+    let height = await images(srcImg).height()
+    if (type_folder === 'swipe') {
+      width = size_data['width']
+      height = size_data['height']
+    } else if (type_folder === 'content') {
+      height = (height/width) * size_data['width']
+      width = size_data['width']
+    }
+    gm(srcImg).resize(width, height).write(srcImg, (err) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve()
+      }
+    })
+  })
 }
 
 module.exports = Ut
--- index.js
+++ index.js
@@ -82,8 +82,8 @@ let dbFuc = async (descImgs, id) => {
 						'Referer': 'https://m.1688.com/',
 					}
 				}
-				let path = `./product/${id}/${arr[i]}/${j}.jpg`
-				let str = await Ut.downImg(opts, path, `./product/${id}/${arr[i]}`)
+				let path = `./${data['folder']}/${id}/${arr[i]}/${j}.jpg`
+				let str = await Ut.downImg(opts, path, `./${data['folder']}/${id}/${arr[i]}`, arr[i], data['size'], data['resize'], id)
 				console.log(str)
 			}
 			catch (e) {
