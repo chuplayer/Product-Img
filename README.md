0c5824d1b3ba989247b65ba1a3fa1bc7dddabd99
yumcc <yumcc@qq.com>
2019-04-09 20:42:40 +0800

    增加速卖通通道

--- README.md
+++ README.md
@@ -2,13 +2,14 @@
 
 第三方模块Puppeteer,mkdirp,request,images,gm</br>
 
-外贸站懒人必备，批量下载1688产品图片(轮播大图+详情图)。</br>
-文件命名为[类别+ID+序号]，避免文件名重复的尴尬。</br>
-可控制批量缩放图片。
+描述：外贸站懒人必备，批量下载1688，速卖通产品图片(轮播大图+详情图)。</br></br>
+更新：文件命名为[类别+ID+序号]，避免文件名重复的尴尬。</br>
+更新：可配置批量控制缩放图片(分轮播图和详情图)。</br>
+更新：增加速卖通通道，批量下载速卖通产品图。</br>
 
 # 使用步骤
-1.直接npm i (Tips: puppeteer的安装可能需要cnpm)</br>
-2.修改data.json里面的id,逐行增加.(https://m.1688.com/offer/588836518552.html 取 588836518552 )<br/>根据需要开启是否进行缩放图片(resize字段).其中swipe是正方形同高同宽需要修改width和height，content为同宽不同高只需要修改width</br>
-3.执行node index.js
+1.直接npm i (Tips: puppeteer的安装可能需要cnpm)。</br>
+2.修改data.json里面的id和type,逐行增加.(ID：https://m.1688.com/offer/588836518552.html 取 588836518552，TYPE：1是1688，2是速卖通 )<br/>根据需要开启是否进行缩放图片(resize字段).其中swipe是正方形同高同宽需要修改width和height，content为同宽不同高只需要修改width。</br>
+3.执行node index.js或者直接双击go.bat。
 
 下载后的图片会在product文件夹里面.
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Ut = require('./common')
 let data = require('./data.json')
 
 //获取阿里巴巴图片url
-let alibaba = async (url, id) => {
+let alibaba = async (url, id, type) => {
 	return new Promise(async (resolve, reject) => {
 		let descImgs = {
 			swipe: [],
@@ -51,6 +51,7 @@ let alibaba = async (url, id) => {
 			return swipe
 		}, swipeHandle)
 		descImgs['swipe'] = swipe
+		// console.log(swipe)
 
 		//采集商品详情图
 		const contentHandle = await page.$('#J_WapDetailCommonDescription_Content')
@@ -72,26 +73,95 @@ let alibaba = async (url, id) => {
 		}, contentHandle)
 		descImgs['content'] = content
 		// console.log(content)
-		await dbFuc(descImgs, id)
+		await dbFuc(descImgs, id, type)
+		return resolve()
+	})
+}
+
+//获取速卖通图片url
+let aliexpress = async (url, id, type) => {
+	return new Promise(async (resolve, reject) => {
+		let descImgs = {
+			swipe: [],
+			content: []
+		}
+
+		// 返回解析为Promise的浏览器
+		const browser = await puppeteer.launch()
+
+		// 返回新的页面对象
+		const page = await browser.newPage()
+		await page.emulate(iPhone)
+
+		// 页面对象访问对应的url地址
+		await page.goto(url)
+
+		// 加载详情图
+		let preScrollHeight = 0
+		let scrollHeight = -1
+		while (preScrollHeight !== scrollHeight) {
+			// 详情信息是根据滚动异步加载，所以需要让页面滚动到屏幕最下方，通过延迟等待的方式进行多次滚动
+			let scrollH1 = await page.evaluate(async () => {
+				let h1 = document.body.scrollHeight
+				window.scrollTo(0, h1)
+				return h1
+			})
+			await page.waitFor(500)
+			let scrollH2 = await page.evaluate(async () => {
+				return document.body.scrollHeight
+			})
+			let scrollResult = [scrollH1, scrollH2]
+			preScrollHeight = scrollResult[0]
+			scrollHeight = scrollResult[1]
+		}
+
+		//采集商品轮播图
+		const swipeHandle = await page.$('.i-amphtml-slides-container')
+		let swipe = await page.evaluate(descContainer => {
+			let swipe = []
+			descContainer.querySelectorAll('.i-amphtml-slide-item > amp-img').forEach(imgElement => {
+				let a = imgElement.getAttribute('src')
+				let b = a.substring(a.indexOf('https'))
+				let c = b.substring(0, b.indexOf('.jpg') + 4)
+				swipe.push(c)
+			})
+			return swipe
+		}, swipeHandle)
+		descImgs['swipe'] = swipe
+		// console.log(swipe)
+
+		//采集商品详情图
+		const frame = page.frames().find(frame => frame.name() === 'amp_iframe0')
+		const contentHandle = await frame.$('body')
+		let content = await frame.evaluate(descContainer => {
+			let content = []
+			descContainer.querySelectorAll('img').forEach(imgElement => {
+				if ((imgElement.getAttribute('src')).indexOf('120x120.jpg') === -1) {
+					content.push(imgElement.getAttribute('src'))
+				}
+			})
+			return content
+		}, contentHandle)
+		descImgs['content'] = content
+		// console.log(content)
+
+		await dbFuc(descImgs, id, type)
 		return resolve()
 	})
 }
 
 // 下载图片到本地
-let dbFuc = async (descImgs, id) => {
+let dbFuc = async (descImgs, id, type) => {
 	let arr = Object.keys(descImgs)
 	for (let i = 0; i < arr.length; i++) {
 		for (let j = 0; j < descImgs[arr[i]].length; j++) {
 			try {
 				let url = descImgs[arr[i]][j]
 				let opts = {
-					url: url,
-					headers: {
-						'Referer': 'https://m.1688.com/',
-					}
+					url: url
 				}
-				let path = `./${data['folder']}/${id}/${arr[i]}/${arr[i]}_${id}_${j}.jpg`
-				let str = await Ut.downImg(opts, path, `./${data['folder']}/${id}/${arr[i]}`, arr[i], data['size'], data['resize'], id)
+				let path = `./${data['folder']}/${type}/${id}/${arr[i]}/${arr[i]}_${id}_${j}.jpg`
+				let str = await Ut.downImg(opts, path, `./${data['folder']}/${type}/${id}/${arr[i]}`, arr[i], data['size'], data['resize'], id)
 				console.log(str)
 			}
 			catch (e) {
@@ -104,7 +174,14 @@ let dbFuc = async (descImgs, id) => {
 (async () => {
 	let arr = data['id']
 	for (let i = 0; i < arr.length; i++) {
-		await alibaba(`https://m.1688.com/offer/${arr[i]}.html`, arr[i])
+		switch (arr[i].type) {
+			case 1:
+				await alibaba(`https://m.1688.com/offer/${arr[i].id}.html`, arr[i].id, 'alibaba')
+				break
+			case 2:
+				await aliexpress(`https://m.aliexpress.com/item/${arr[i].id}.html`, arr[i].id, 'aliexpress')
+				break
+		}
 		if (i + 1 === arr.length) {
 			console.log('全部下载完成，请关闭控制台！')
 		}
